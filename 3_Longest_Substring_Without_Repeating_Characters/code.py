4b983d891b41d46c91e6c91cc44d46b2319af088
Raintown <403432819@qq.com>
2018-01-31 01:42:51 +0800

    Add the 3 promblem

---
+++ 3_Longest_Substring_Without_Repeating_Characters/code.py
@@ -0,0 +1,11 @@
+class Solution(object):
+	def lengthOfLongestSubstring(self,s):
+		start,cha_dict,result,tp_len=0,{},0,0
+		for it in range(len(s)):
+			sit=s[it]
+			if sit in cha_dict and cha_dict[sit] >=start:
+				start=cha_dict[sit]+1
+			else:
+				result=max(it - start +1,result)
+			cha_dict[sit]=it
+		return result
