4537c9f516baae747da15ebbad967ab2d79a869a
Raintown <403432819@qq.com>
2018-02-27 19:44:10 +0800

    Commit 39

--- 39_Combination_Sum/code.py
+++ 39_Combination_Sum/code.py
@@ -1,8 +1,20 @@
 class Solution(object):
-    def combinationSum(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
-        
+	def combinationSum(self, candidates, target):
+		"""
+		:type candidates: List[int]
+		:type target: int
+		:rtype: List[List[int]]
+		"""
+		def sub_array(arr,tar,loc):
+			sub_result=[]
+			for item in range(loc,len(arr)):
+				if arr[item]<tar:
+					tp=sub_array(arr,tar-arr[item],item)
+					for tmp in tp:
+						sub_result.append([arr[item]]+tmp)
+				elif arr[item]==tar:
+					sub_result.append([arr[item]])
+				else:
+					break
+			return sub_result
+		return sub_array(sorted(candidates),target,0)
